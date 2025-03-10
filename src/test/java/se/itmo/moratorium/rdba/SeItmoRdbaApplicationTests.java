6bb4456a4caa4ee89bd3e9fab774ab6bda19a1f3
moratorium <spgamermdk@gmail.com>
2025-03-10 20:00:23 +0300

    init

---
+++ src/test/java/se/itmo/moratorium/rdba/SeItmoRdbaApplicationTests.java
@@ -0,0 +1,13 @@
+package se.itmo.moratorium.rdba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeItmoRdbaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
