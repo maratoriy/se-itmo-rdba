0822a3287b2eac204c267fa6c843c7196133d6f1
moratorium <spgamermdk@gmail.com>
2025-03-10 21:28:00 +0300

    added project entity

---
+++ src/main/java/se/itmo/moratorium/rdba/model/projects/ProjectEntity.java
@@ -0,0 +1,36 @@
+package se.itmo.moratorium.rdba.model.projects;
+
+import lombok.*;
+import jakarta.persistence.*;
+import se.itmo.moratorium.rdba.model.tasks.TaskEntity;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "projects")
+@NamedEntityGraph(
+        name = "Project.withTasks",
+        attributeNodes = {
+                @NamedAttributeNode("tasks")
+        }
+)
+public class ProjectEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "project", fetch = FetchType.LAZY)
+    private Set<TaskEntity> tasks;
+}
